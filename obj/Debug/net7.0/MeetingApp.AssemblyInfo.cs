5ff42bd92ba9aede874344dff9993bd70aceefa1
Oğuzhan Kandemir <oguzhankandemir11@gmail.com>
2024-06-04 13:12:03 +0300

    Meeting App Asp .Net Core 7
    Meeting App Asp .Net Core 7 sürümü ile geliştirilmiştir.

--- obj/Debug/net7.0/MeetingApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/MeetingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80c463403f2c5ed43b18fe0960bc1cdc83e05ffb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
